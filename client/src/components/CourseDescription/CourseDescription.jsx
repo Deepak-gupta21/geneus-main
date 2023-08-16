731a255f451cad5af33ce9fda45e4bde226cd32c
nehabn11 <neha.bn@geneussolutions.in>
2023-08-16 13:19:21 +0530

    added course description details

---
+++ client/src/components/CourseDescription/CourseDescription.jsx
@@ -0,0 +1,177 @@
+import React from 'react';
+import {
+  MDBContainer, 
+  MDBRow, 
+  MDBCol,
+  MDBCard,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCardBody,
+} from 'mdb-react-ui-kit';
+import './CourseDescription.css';
+
+function CourseDescription() {
+  return (
+    <MDBContainer>
+      <MDBRow md='2'>
+
+      <MDBCard style={{ maxWidth: '2000px' }}>
+      <MDBRow className='g-8'>
+        <MDBCol md='8'>
+          <MDBCardBody>
+            <MDBCardTitle className="mt-2 text-dark fs-2 fw-bold">React JS- Complete Guide for Frontend Web Development</MDBCardTitle>
+          </MDBCardBody>
+          <MDBCardText className="fs-6 fw-normal text-dark">
+            <div className='div-margin'>
+            <p>Become an expert React JS Developer. Learn HTML, CSS, JavaScript, ES6, React JS and jQuery.</p>
+            </div>
+
+          </MDBCardText>
+        </MDBCol>
+      </MDBRow>
+    </MDBCard>
+
+    <hr className='hr'/>
+
+      <MDBCard style={{ maxWidth: '1000px' }}>
+      <MDBRow className='g-0'>
+        <MDBCol md='10'>
+          <MDBCardBody>
+            <MDBCardTitle className="mt-2 text-dark fs-4 fw-bold">What you'll learn</MDBCardTitle>
+            
+            <MDBCardText className="fs-6 fw-normal">
+            <div>
+            <div className='div1'> 
+            <ul className='ticks'>
+              <li >Learn to create basic web pages using HTML and HTML5</li><br/>
+              <li >Learn to style and add animations to bring those basic web pages to life using CSS3</li><br/>
+              <li >Create user-friendly, beautiful, light and fast websites using React JS</li><br/>              
+            </ul>
+            </div> 
+            <div className='div2'>
+            <ul className='ticks'>
+              <li >Learn the Javascript language from basic to advanced along with the Next Gen JS - ES6</li><br/>
+              <li >Learn to create responsive and interactive web pages using Javascript and jQuery</li><br/>
+              <li >Become an expert front end developer using ReactJs</li><br/>
+            </ul>
+            </div>
+            </div>
+            </MDBCardText>
+            
+          </MDBCardBody>
+        </MDBCol>
+      </MDBRow>
+    </MDBCard>
+
+    <hr className='hr'/>
+
+    <MDBCard style={{ maxWidth: '1000px' }}>
+      <MDBRow className='g-8'>
+        <MDBCol md='10'>
+          <MDBCardBody>
+            <MDBCardTitle className="mt-2 text-dark fs-4 fw-bold">Requirements</MDBCardTitle>
+          </MDBCardBody>
+          <MDBCardText className="fs-6 fw-normal">
+            <div className='div-margin'>
+              <ul>
+                <li>Should have basic knowledge of operating Computer</li>
+              </ul>
+            </div>
+          </MDBCardText>
+        </MDBCol>
+      </MDBRow>
+    </MDBCard>
+
+    <hr className='hr'/>
+
+    <MDBCard style={{ maxWidth: '1000px' }}>
+      <MDBRow className='g-8'>
+        <MDBCol md='10'>
+          <MDBCardBody>
+            <MDBCardTitle className=" mt-2 text-dark fs-4 fw-bold">Description</MDBCardTitle>
+          </MDBCardBody>
+          <MDBCardText className="fs-6 fw-normal">
+          <div style={{margin: "3px"}}>
+            <div className='div-margin'>
+              <h5><b>What's this course about?</b></h5>
+              <p>
+              This course is a complete guide to become an expert <b>front-end web developer</b>. It contains all the tools and technologies that you should be proficient in work as a <b>React Developer.</b>
+              </p>
+            </div>
+            <div className='div-margin'>
+              <ul>
+                <li>We will go through the journey of learning <b>HTML5, CSS3, Javascript, ES6, jQuery, and React JS.</b></li>
+                <li>Each of these course modules has a project which you can work on.</li>
+                <li>Along with the project problem statement video, you will also find the solution video for that project. It is advised to first try working on the project by yourself.</li>
+                <li>Once you have completed working on it,  you can see the solution video to know what are the best practices and the most optimized way to complete it.</li>
+                <li>As you keep on creating new projects, start adding them to your GitHub repository so that by the end of the course you will be having a place to showcase all your work.</li>
+                <li>You will definitely enjoy the journey while completing the course!</li>
+              </ul>
+            </div>
+          </div>
+          
+          <br/>
+          <div>
+            <div className='div-margin'>
+              <h5><b>Why ReactJs?</b></h5>
+              <p>
+              You know Companies Worldwide Require <b>React JS Developers.</b>
+              <br/>
+              React.js is one of the most demanding frameworks in the market;31.3% of worldwide specialists are currently utilizing ReactJs. Some of the world’s top tech teams use the tool to design scalable, easy-to-maintain solutions — Netflix, PayPal, eBay, Airbnb, and more. Why are businesses so interested in React.js implementation? Here are the benefits of adopting the framework:
+              </p>
+            </div>
+            <div className='div-margin'>
+              <ul>
+                <li><b>Increased app development speed.</b> The framework accepts HTML code input and has excellent subcomponent rendering support. Development teams can benefit from JSX as well — it’s an extension with a range of shortcuts that cuts the amount of code writing time.</li>
+                <li><b>Improved code stability.</b>React.js reduces the impact of change in the code file since the changes in parent components don’t affect the children. Such a data binding approach facilitates software testing and helps ensure stable app performance after update deployments.</li>
+                <li><b>SEO-friendliness.</b>React.js deals with the inability of search engine crawlers to process JS-heavy pages. Even a beginner with an entry-level react developer salary can deploy React on a server and use the virtual DOM to view the code as a regular web page in a browser.</li>
+                <li><b>Robust toolkit.</b> React.js has a wide range of debugging and interface design tools.</li>
+                <li><b>Strong user community and an affordable remote react developer salary. </b> Since a large number of teams are deploying the framework, the tool is regularly updated. There’s no shortage of user-generated content either — you can find dozens of helpful React tutorials and case studies to make the most out of the framework.</li>
+              </ul>
+            </div>
+            <div className='div-margin'>
+              <p>
+              Asking for a Senior react developer salary. It’s one of the most in-demand frameworks at the moment — that’s why business owners are willing to find professionals with a moderate mid-level react developer salary within and beyond their home regions.
+              <br/>
+              <b>Enroll now!! see you in class.
+                <br/>
+                Happy Learning
+                <br/>
+                Team Geneus
+              </b>
+              </p>
+            </div>
+          </div>
+          </MDBCardText>
+        </MDBCol>
+      </MDBRow>
+    </MDBCard>
+
+    <hr className='hr'/>
+
+    <MDBCard style={{ maxWidth: '1000px' }}>
+      <MDBRow className='g-8'>
+        <MDBCol md='10'>
+          <MDBCardBody>
+            <MDBCardTitle className="mt-2 text-dark fs-4 fw-bold">Who this course is for:</MDBCardTitle>
+          </MDBCardBody>
+          <MDBCardText className="fs-6 fw-normal">
+            <div className='div-margin'>
+              <ul>
+                <li>Beginners who want to learn Web Development from scratch</li>
+                <li>Freshers who want to become a React Frontend Developer</li>
+                <li>Programmers who want to learn Javascript language</li>
+                <li>Developers who want to learn React JS</li>
+              </ul>
+            </div>
+          </MDBCardText>
+        </MDBCol>
+      </MDBRow>
+    </MDBCard>
+        
+      </MDBRow>
+    </MDBContainer>
+  );
+}
+
+export default CourseDescription;
